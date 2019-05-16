7142597d39c6cb0a936eb59d3af6466dd29eeceb
Jessika Willem frey <jwillem-@oa-d1.21-school.ru>
2019-05-16 21:17:38 +0300

    Norme

--- srcs/ant_flow.c
+++ srcs/ant_flow.c
@@ -6,63 +6,11 @@
 /*   By: jwillem- <jwillem-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 18:24:28 by jwillem-          #+#    #+#             */
-/*   Updated: 2019/05/16 17:12:44 by jwillem-         ###   ########.fr       */
+/*   Updated: 2019/05/16 21:14:45 by jwillem-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
-
-static void	make_best_combination(t_map *map)
-{
-	t_list	*comb_ptr;
-	t_list	*way;
-	size_t	comb_number;
-	int		sum_len;
-	int		cur_steps;
-	int		way_q;
-
-	comb_ptr = map->way;
-	C_STEPS = INT_MAX;
-	while (comb_ptr)
-	{
-		way = comb_ptr->content;
-		sum_len = 0;
-		way_q = 0;
-		while (way)
-		{
-			sum_len += ft_lstlen(way->content) - 1;
-			cur_steps = (map->ants + sum_len - 1) / ++way_q;
-			if (cur_steps < C_STEPS)
-			{
-				comb_number = comb_ptr->content_size;
-				C_QUANT = way_q;
-				C_STEPS = cur_steps;
-				C_SUM_LEN = sum_len;
-			}
-			way = way->next;
-		}
-		comb_ptr = comb_ptr->next;
-	}
-	comb_ptr = map->way;
-	while (comb_ptr->content_size != comb_number)
-		comb_ptr = comb_ptr->next;
-	way = comb_ptr->content;
-	SECURE_MALLOC(C_WAY = ft_memalloc(sizeof(t_path) * (C_QUANT + 1)));
-	way_q = -1;
-	while (++way_q < C_QUANT)
-	{
-		C_WAY[way_q].len = ft_lstlen(way->content) - 1;
-		SECURE_MALLOC(C_WAY[way_q].rooms = ft_memalloc(sizeof(t_room*) * (C_WAY[way_q].len + 1)));
-		t_list	*ptr = way->content;
-		int	i = -1;
-		while (ptr->next)
-		{
-			C_WAY[way_q].rooms[++i] = ptr->next->content;
-			ptr = ptr->next;
-		}
-		way = way->next;
-	}
-}
 
 static int	sum_difference(t_map *map, int i)
 {
@@ -108,40 +56,46 @@ static void	print_ant(int ant, char *room, int cycle)
 	ft_putstr(number);
 	write(1, "-", 1);
 	ft_putstr(room);
-	// write(1, " ", 1);
 	free(number);
 }
 
-static void	let_ants_go(t_map *map)
+static void	show_ants_locations(t_map *map, int wi, int *cycle)
 {
-	int	ant;
+	static int	ant;
+	int			ri;
+
+	ri = C_WAY[wi].last_i + 1;
+	while (--ri >= 0 && map->ants)
+	{
+		if (ri == 0 && C_WAY[wi].ants)
+		{
+			C_WAY[wi].rooms[ri]->ant_num = ++ant;
+			C_WAY[wi].ants--;
+		}
+		else if (ri > 0 && C_WAY[wi].rooms[ri - 1]->ant_num)
+		{
+			C_WAY[wi].rooms[ri]->ant_num = C_WAY[wi].rooms[ri - 1]->ant_num;
+			C_WAY[wi].rooms[ri - 1]->ant_num = 0;
+		}
+		else
+			break ;
+		print_ant(C_WAY[wi].rooms[ri]->ant_num, \
+			C_WAY[wi].rooms[ri]->name, ++(*cycle));
+	}
+}
+
+void		ant_flow(t_map *map)
+{
 	int	wi;
-	int	ri;
 	int	cycle;
 	
-	ant = 1;
+	make_best_combination(map);
+	distribute_ants(map);
 	wi = -1;
 	cycle = -1;
 	while (++wi < C_QUANT && map->ants)
 	{
-		
-		ri = C_WAY[wi].last_i + 1;
-		while (--ri >= 0 && map->ants)
-		{
-			if (ri == 0 && C_WAY[wi].ants)
-			{
-				C_WAY[wi].rooms[ri]->ant_num = ant++;
-				C_WAY[wi].ants--;
-			}
-			else if (ri > 0 && C_WAY[wi].rooms[ri - 1]->ant_num)
-			{
-				C_WAY[wi].rooms[ri]->ant_num = C_WAY[wi].rooms[ri - 1]->ant_num;
-				C_WAY[wi].rooms[ri - 1]->ant_num = 0;
-			}
-			else
-				break ;
-			print_ant(C_WAY[wi].rooms[ri]->ant_num, C_WAY[wi].rooms[ri]->name, ++cycle);
-		}
+		show_ants_locations(map, wi, &cycle);
 		if (C_WAY[wi].last_i < C_WAY[wi].len - 1)
 			C_WAY[wi].last_i++;
 		else
@@ -153,13 +107,4 @@ static void	let_ants_go(t_map *map)
 			write(1, "\n", 1);
 		}
 	}
-}
-
-void    ant_flow(t_map *map)
-{
-	make_best_combination(map);
-	// print_one_comb(map);	//debug
-	distribute_ants(map);
-	// print_best_comb(map);	//debug
-	let_ants_go(map);
 }
